e3ac52ef3bd2debb90e0d533c7efcbdda46ec2ff
Christian Romberg <distjubo@gmail.com>
2020-05-13 11:45:37 +0200

    switch to uncalibrated magnetic field sensor

--- app/src/main/java/org/itoapp/fieldtest/datasource/MagneticField.java
+++ app/src/main/java/org/itoapp/fieldtest/datasource/MagneticField.java
@@ -13,7 +13,8 @@ import static android.content.Context.SENSOR_SERVICE;
 public class MagneticField implements DataSource, SensorEventListener {
     private SensorManager sensorManager;
     private int accuracy;
-    private float magnetic_x, magnetic_y, magnetic_z;
+    private float magnetic_x = Float.NaN, magnetic_y = Float.NaN, magnetic_z = Float.NaN;
+    private float magnetic_bias_x = Float.NaN, magnetic_bias_y = Float.NaN, magnetic_bias_z = Float.NaN;
 
     @Override
     public boolean setup(Context context) {
@@ -23,7 +24,7 @@ public class MagneticField implements DataSource, SensorEventListener {
             return false;
         }
 
-        Sensor magnetometer = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        Sensor magnetometer = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD_UNCALIBRATED);
 
         if (magnetometer == null) {
             return false;
@@ -36,17 +37,23 @@ public class MagneticField implements DataSource, SensorEventListener {
 
     @Override
     public String[] getDataLabels() {
-        return new String[]{"magnetic_x", "magnetic_y", "magnetic_z", "magnetic_accuracy"};
+        return new String[]{"magnetic_x", "magnetic_y", "magnetic_z",
+                "magnetic_bias_x", "magnetic_bias_y", "magnetic_bias_z",
+                "magnetic_accuracy"};
     }
 
     @Override
     public Type[] getDataTypes() {
-        return new Type[]{float.class, float.class, float.class, int.class};
+        return new Type[]{float.class, float.class, float.class,
+                float.class, float.class, float.class,
+                int.class};
     }
 
     @Override
     public Object[] getData() {
-        return new Object[]{magnetic_x, magnetic_y, magnetic_z, accuracy};
+        return new Object[]{magnetic_x, magnetic_y, magnetic_z,
+                magnetic_bias_x, magnetic_bias_y, magnetic_bias_z,
+                accuracy};
     }
 
     @Override
@@ -59,6 +66,9 @@ public class MagneticField implements DataSource, SensorEventListener {
         magnetic_x = event.values[0];
         magnetic_y = event.values[1];
         magnetic_z = event.values[2];
+        magnetic_bias_x = event.values[3];
+        magnetic_bias_y = event.values[4];
+        magnetic_bias_z = event.values[5];
         accuracy = event.accuracy;
     }
 
